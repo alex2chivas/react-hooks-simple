c3a956577d440d575076ae2a8847c5a1a970ff7e
Alexis Flores <alex2chivas@hotmail.com>
2020-04-07 16:49:26 -0700

    initial setup for App and imported useState

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 
-const App = () => {
-	return <div>App</div>;
-};
+class App extends Component {
+	state = { resource: 'posts' };
+
+	render() {
+		return (
+			<div className='container' style={{ padding: '20px' }}>
+				<div>
+					<div>
+						<button
+							className='positive ui button'
+							onClick={() => this.setState({ resource: 'posts' })}>
+							Posts
+						</button>
+						<button
+							className='negative ui button'
+							onClick={() => this.setState({ resource: 'todos' })}>
+							Todos
+						</button>
+					</div>
+				</div>
+				{this.state.resource}
+			</div>
+		);
+	}
+}
 
 export default App;
