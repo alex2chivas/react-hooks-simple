b7eeb2183b285798a324f1ef04d746c5364a8991
Alexis Flores <alex2chivas@hotmail.com>
2020-04-08 14:39:18 -0700

    changed the async call from being a reference to calling itself with the double parenthesis

--- src/component/ResourceList.js
+++ src/component/ResourceList.js
@@ -4,15 +4,20 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
 	const [ resources, setResources ] = useState([]);
 
-	const fetchResource = async resource => {
-		const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+	//Note - one way doign the axios call and passing the value into the useEffect()
+	// const fetchResource = async resource => {
+	// 	const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-		setResources(response.data);
-	};
+	// 	setResources(response.data);
+	// };
 
 	useEffect(
 		() => {
-			fetchResource(resource);
+			(async resource => {
+				const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+
+				setResources(response.data);
+			})(resource);
 		},
 		[ resource ]
 	);
