b4e5aaefc3b16bf539d0573296641dd1b7fc8d9d
Alexis Flores <alex2chivas@hotmail.com>
2020-04-08 21:13:25 -0700

    converted the axios call into a seperate functional call to make the code reusable across the application

--- src/component/ResourceList.js
+++ src/component/ResourceList.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
 	const [ resources, setResources ] = useState([]);
-	//Note - one way doign the axios call and passing the value into the useEffect()
-	// const fetchResource = async resource => {
-	// 	const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-	// 	setResources(response.data);
-	// };
 	useEffect(
 		() => {
 			(async resource => {
@@ -19,6 +14,12 @@ const ResourceList = ({ resource }) => {
 		},
 		[ resource ]
 	);
+
+	return resources;
+};
+
+const ResourceList = ({ resource }) => {
+	const resources = useResources(resource);
 
 	return <ul>{resources.map(record => <li key={record.id}> {record.title} </li>)}</ul>;
 };
